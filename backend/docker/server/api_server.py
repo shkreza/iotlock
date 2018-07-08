2580e0facc8f8c1322153dcfe0ac5b1a1d3032ed
Reza Sherafat <Pacebox1@gmail.com>
2018-07-08 15:14:41 -0700

    Fixed web view to work with cross sites.

--- backend/docker/server/api_server.py
+++ backend/docker/server/api_server.py
@@ -59,10 +59,24 @@ def requires_hub_name(f):
             return f(hub_name, *args, **kwargs)
     return decorated_requires_hub_name
 
-@app.route(API_BASE_PATH + '/lock', methods=['POST'])
+def add_allow_cross_reference_headers(f):
+    @wraps(f)
+    def decorated_add_cross_reference_headers(*args, **kwargs):
+        response = f(*args, **kwargs)
+        response.headers['Access-Control-Allow-Origin'] = '*'
+        response.headers['Access-Control-Allow-Methods'] = 'POST, GET, OPTIONS'
+        response.headers['Access-Control-Max-Age'] = 1000
+        # note that '*' is not valid for Access-Control-Allow-Headers
+        response.headers['Access-Control-Allow-Headers'] = 'origin, x-csrf-token, content-type, accept, Authorization, ' + ', '.join(list(HEADER_PARAMS.values()))
+        return response
+    return decorated_add_cross_reference_headers
+
+@app.route(API_BASE_PATH + '/lock', methods=['POST'], provide_automatic_options=False)
 @requires_device_id
 @requires_hub_name
+@add_allow_cross_reference_headers
 def api_update_lock(hub_name, device_id):
+    print('Processing POST request...')
     method_response = invoke_device(hub_name, device_id, METHOD_NAME)
     if not method_response:
         return make_error_response('Error occurred: {}'.format('Invocation failed; device might be offline.'), 500)
@@ -71,13 +85,21 @@ def api_update_lock(hub_name, device_id):
     else:
         return make_error_response('Error occurred: {}'.format(method_response.payload), method_response.status)
 
-@app.route(API_BASE_PATH + '/lock', methods=['GET'])
+@app.route(API_BASE_PATH + '/lock', methods=['GET'], provide_automatic_options=False)
 @requires_device_id
 @requires_hub_name
+@add_allow_cross_reference_headers
 def api_get_lock_state(hub_name, device_id):
+    print('Processing GET request...')
     assert(device_id)
     assert(hub_name)
     
     message = 'Found these parameters: hub_name={}, device_id={}'.format(hub_name, device_id)
     return make_response(jsonify({'message': message}), 200)
 
+@app.route(API_BASE_PATH + '/lock', methods=['OPTIONS'])
+@add_allow_cross_reference_headers
+def corss_reference_options():
+    print('Processing OPTIONS request...')
+    response = make_response()
+    return response
